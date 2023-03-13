cd145d7c90b4073f4812a2f588cd2d799fc762bd
Rahul Guni <rguni@aquent.com>
2023-03-13 14:12:17 -0400

    added cosine similarity

--- talent-techies/routes/index.js
+++ talent-techies/routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const similarity = require('cosine-similarity');
 const { Configuration, OpenAIApi } = require('openai');
 require('dotenv').config();
 const configuration = new Configuration({
@@ -9,18 +10,16 @@ const openai = new OpenAIApi(configuration);
 var router = express.Router();
 
 
-router.get('/key', (req, res) => {
-  res.send(openai);
-})
-
 router.post('/api/benefits-answers', async (req, res) => {
   const question = req.body.question;
+
+  const genericResponse = 'That is a great question, unfortunately we don\'t have any data to answer that..'
 
   const options = {
     model: process.env.MODEL,
     prompt: question,
     max_tokens: 256,
-    temperature: 0.5,
+    temperature: 0.1,
     top_p: 1,
     frequency_penalty: 0,
     presence_penalty: 0
@@ -30,10 +29,37 @@ router.post('/api/benefits-answers', async (req, res) => {
   var answer = response.data.choices[0].text;
 
   if(answer.indexOf('\n')) {
-    answer = answer.slice(0, answer.indexOf('\n\n'))
+    answer = answer.slice(0, answer.indexOf('\n'));
   }
-  res.send(answer);
+
+  if((checkSimilarity(question, answer)) > 0.4){
+    res.send(answer);
+  } 
+  else {
+    res.send(genericResponse);
+  }
 
 });
+
+function checkSimilarity(question, answer) {
+  const inputTokens = question.split(' ');
+  const generatedTokens = answer.split(' ');
+
+  //create a vector of the input question and generated response using the word frequencies
+  const inputVector = inputTokens.reduce((vector, word) => {
+    vector[word] = (vector[word] || 0) + 1;
+    return vector;
+  }, {});
+
+  const generatedVector = generatedTokens.reduce((vector, word) => {
+    vector[word] = (vector[word] || 0) + 1;
+    return vector;
+  }, {});
+
+  // calculate the cosine similarity between the input question and generated response
+  const cosineSimilarity = similarity(Object.values(inputVector), Object.values(generatedVector));
+
+  return cosineSimilarity;
+}
 
 module.exports = router;
